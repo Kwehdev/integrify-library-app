99d76fe61e1c3d986a0bcdf9cb0a8035c463a3fc
KwehDev <Kwehdev@protonmail.com>
2020-10-09 13:22:51 +0300

    Integrated SendGrid for emails

--- app/src/server/utils/emailUtils.ts
+++ app/src/server/utils/emailUtils.ts
@@ -1,4 +1,14 @@
-export const sendEmail = async (email: Email) => {}
+import sgMail from '@sendgrid/mail'
+sgMail.setApiKey(process.env.SENDGRID_API_KEY)
+
+export const sendEmail = async (email: Email) => {
+  console.log('sending')
+  try {
+    await sgMail.send(email)
+  } catch (err) {
+    console.error(err)
+  }
+}
 
 type EmailConstructor = (
   firstName: string,
@@ -7,13 +17,28 @@ type EmailConstructor = (
   resetTokenExpiry: number
 ) => Email
 
-type Email = {}
+type Email = {
+  to: string
+  from: string
+  subject: string
+  html: string
+}
 
+//In future this will be an endpoint to reset a password.
 export const constructPasswordRequestEmail: EmailConstructor = (
   firstName,
   email,
   resetToken,
   resetTokenExpiry
 ) => {
-  return {}
+  return {
+    to: email,
+    from: 'kwehdev@protonmail.com',
+    subject: 'Your LibraryApp password reset request',
+    html: `<h4>Hello, ${firstName},
+    We recently received a request to reset your password.
+    Access token: ${resetToken},
+    Expires In: ${resetTokenExpiry}
+    `,
+  }
 }
